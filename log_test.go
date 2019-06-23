a7e8c8cab80733de317d74c649df15fc03d57dc6
slonegd <slonkdv@mail.ru>
2019-06-23 08:50:41 +0300

    add test from readme

--- log_test.go
+++ log_test.go
@@ -15,7 +15,22 @@ func TestLogOtusEvent(t *testing.T) {
 		event log.OtusEvent
 		want  string
 	}{
-		// TODO: Add test cases.
+		{
+			name: "from readme 1",
+			event: log.HwSubmitted{
+				Id:      3456,
+				Comment: "please take a look at my homework",
+			},
+			want: `2019-01-01 submitted 3456 "please take a look at my homework"`,
+		},
+		{
+			name: "from readme 2",
+			event: log.HwAccepted{
+				Id:    3456,
+				Grade: 4,
+			},
+			want: `2019-01-01 accepted 3456 4`,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
